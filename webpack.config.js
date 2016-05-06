8cd510518a16bf3abdd5c9f21b136fc2a749b1c9
Dan Zajdband <dan.zajdband@gmail.com>
2016-05-06 11:09:13 -0400

    targeting node and the browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'index.js',
-    path: './dist',
-    libraryTarget: "commonjs2"
+module.exports = [
+  {
+    entry: './src/index.js',
+    output: {
+      filename: 'browser.js',
+      path: './dist',
+      libraryTarget: "commonjs2",
+      target: "browser"
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      ]
+    }
   },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-    ]
+  {
+    entry: './src/index.js',
+    output: {
+      filename: 'node.js',
+      path: './dist',
+      libraryTarget: "commonjs2",
+      target: "node"
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      ]
+    }
   }
-}
+]
